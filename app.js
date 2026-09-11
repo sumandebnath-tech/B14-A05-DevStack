f3bbcc5601057cbc08de4b66841e2b3612bf7aab
Suman Debnath <samudm81@gmail.com>
2026-09-12 01:58:02 +0600

    feat: add mobile menu toggle functionality in app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Toggle Mobile Menu
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', () =>{
+        navLinks.classList.toggle('active');
+    });
+}
